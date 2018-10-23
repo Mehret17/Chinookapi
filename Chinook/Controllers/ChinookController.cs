87cb44c31cd7c9c68dda53b73f409d0c91f5c74d
Mehret Kulala <merutaye@gmail.com>
2018-10-23 18:11:18 -0500

    completed ex-1

---
+++ Chinook/Controllers/ChinookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Chinook.DataAccess;
+
+namespace Chinook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChinookController : ControllerBase
+    {
+        private readonly ChinookStorage _storage;
+
+        public ChinookController()
+        {
+            _storage = new ChinookStorage();
+        }
+
+        [HttpGet]
+        public IActionResult GetSales()
+        {
+            return Ok(_storage.GetSalesAgent());
+        }
+    }
+}
---
+++ Chinook/DataAccess/ChinookStorage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Chinook.Models;
+
+namespace Chinook.DataAccess
+{
+    public class ChinookStorage
+    {
+        private const string ConnectionString = "Server=localhost;Database=Chinook;Trusted_Connection=True;";
+        public List<SalesAgents> GetSalesAgent()
+        {
+
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                db.Open();
+                var command = db.CreateCommand();
+                command.CommandText = @"Select E.FirstName + ' ' + E.LastName as Name, InvoiceId
+                                               from Employee E
+                                                join Customer C on E.EmployeeId = C.SupportRepId
+                                                join Invoice I on C.CustomerId = I.CustomerId
+                                                Group by E.FirstName, E.LastName, InvoiceId";
+
+               // command.Parameters.AddWithValue("@id", id);
+
+                var reader = command.ExecuteReader();
+
+                var employees = new List<SalesAgents>();
+
+                while (reader.Read())
+                {
+                    var employee = new SalesAgents
+                    {
+                        InvoiceId = (int)reader["InvoiceId"],
+                        Name = reader["Name"].ToString(),
+                    };
+
+                    employees.Add(employee);
+                }
+
+                return employees;
+            }
+        }
+    }
+}
